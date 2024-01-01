a6cd199b703ccddf4b6322d73f0aed9825b7d969
francis-tg <franc9885@gmail.com>
2024-01-01 07:00:33 +0000

    custom repo

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
-const gitHubToken = "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";
+const gitHubToken = process.env.GIT_TOKEN || null;
 const username = "francis-tg";
 const url = `https://api.github.com/users/${username}/repos`;
+console.log(process.env);
 export function fetchRepo() {
 	return fetch(url, {
 		method: "GET",
--- src/components/Repsitoriy.jsx
+++ src/components/Repsitoriy.jsx
@@ -4,13 +4,19 @@ import Icon from "./Icon";
 
 function Repsitoriy({ repositorie }) {
 	return (
-		<div className='border border-yellow-500 rounded p-5'>
-			<div>
+		<div className='border border-yellow-500 rounded p-5 cursor-pointer'>
+			<div className='flex flex-col items-start justify-between gap-3 mb-3'>
 				<h1 className='text-lg font-bold'>{repositorie?.name}</h1>
 				<p>{String(repositorie?.description ?? "").substring(0, 100) || ""}</p>
 			</div>
-			<div className='flex justify-between'>
-				<div></div>
+			<div className='flex justify-between  items-center'>
+				<div>
+					<img
+						className='w-8 rounded-full'
+						src={repositorie?.owner?.avatar_url}
+						alt=''
+					/>
+				</div>
 				<div className='flex items-center gap-2'>
 					<div>
 						<Icon
